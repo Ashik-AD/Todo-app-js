e7bccfa878fe868050ac0ea332badf200ebf69ab
Ashik-AD <43263257+Ashik-AD@users.noreply.github.com>
2021-01-26 22:01:38 +0545

    Final Wrap-up

--- main.js
+++ main.js
@@ -160,6 +160,11 @@ const App = function () {
     }
     const renderList = (arr) => {
         DOMS.todoListWrp.innerHTML = '';
+        console.log(arr)
+        if (!arr.length) {
+            DOMS.todoListWrp.innerHTML = `<h1 class="first-time">You can add your task clicking above typing box.🙂💙❤</h1>`;
+            DOMS.input.focus();
+        }
         arr.forEach(el => {
             DOMS.todoListWrp.insertAdjacentHTML('beforeend', htmlMarkup(el));
         })
